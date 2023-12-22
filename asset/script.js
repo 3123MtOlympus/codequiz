264acea57edc03c0ab27c9dd6bd11566cc602a1b
3123MtOlympus <realchrislondon@gmail.com>
2023-12-21 22:28:15 -0800

    fixing functionality

--- asset/script.js
+++ asset/script.js
@@ -1,78 +1,54 @@
 const startBtn = document.querySelector('button');
+const nextBtn = document.getElementById("next-btn")
+const questionContainerElement = document.getElementById("question-container")
+let shuffledQuestions, currentQuestion
+const questionElement = document.getElementById("question")
 
+function onStartBtnClick(){
+    alert("Click OK to test your knowledge on javascript!");
+    startBtn.addEventListener("click", onStartBtnClick, startGame);
+}
+function startGame() {
+    startBtn.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestion = 0;
+    questionContainerElement.classList.remove("hide");
 
-function onStartBtnClick() {
-    alert("Click OK to begin quiz.");   
+}
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestion]);
+}
+
+function showQuestion(){
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct){
+            button.dataset.correct = answer.correct
+        }
+          })
+}
+function resetState() {
+    nextBtn.classList.add("hide");
 }
 
 function onSubmitBtnClick() {
-    alert("You've completed your quiz! Let's see your results!")
+    alert("You've completed your quiz! Let's see your results!");
+    submitBtn.addEventListener('click',onSubmitBtnClick, showResults);
 }
 
-startBtn.addEventListener('click', onStartBtnClick);
-submitBtn.addEventListener('click',onSubmitBtnClick, showResults);
+
 
 const quizContainer = document.getElementById('quiz-question-container');
 const resultsContainer = document.getElementById('results');
 const submitBtn = document.getElementById('submit');
 
-
-function buildQuiz() { 
-    const output =[];
-
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-
-            const answers = [];
-
-            for(letter in currentQuestion.answers){
-
-                answers.push(
-                    `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                    </label>`
-                );
-            }
-            output.push(
-               `<div class="questions"> ${currentQuestion.question} </div>
-               <div class="answers">${answers.join('')} </div>` 
-            );
-        }
-    );
-    quizContainer.innerHTML =output.join('');
-}
-
-function showResults() { 
-    const answerContainers = quizContainer.querySelectorAll('answers');
-
-    let numCorrect = 0;
-
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]: checked`;
-        const userAnswer = (answerContainer.querySelector(selector)||{}).value;
-
-        if(userAnswer === currentQuestion.correctAnswer){
-            numCorrect++;
-
-            answerContainers[questionNumber].style.color = 'lightgreen';
-        } else{
-            answerContainers[questionNumber].style.color = 'red';
-        }
-
-});
-
-resultsContainer.innerHTML = `${numCorrect} out of ${myQuestion.length}`;
-}
-
-buildQuiz();
-
-submitBtn.addEventListener('click', showResults);
-
-const myQuestions = [{
+const myQuestions = [
+    {
     question: "Javascript is a _______ langauge?",
     answers: {
         A: "Object-Oriented",
@@ -125,3 +101,62 @@ const myQuestions = [{
 }
 
 ];
+
+function buildQuiz() { 
+    const output =[];
+
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            const answers = [];
+
+            for(letter in currentQuestion.answers){
+
+                answers.push(
+                    `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                    ${letter} :
+                    ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+            output.push(
+               `<div class="questions"> ${currentQuestion.question} </div>
+               <div class="answers">${answers.join('')} </div>` 
+            );
+        }
+    );
+    quizContainer.innerHTML =output.join('');
+}
+
+buildQuiz();
+
+
+function showResults() { 
+    const answerContainers = quizContainer.querySelectorAll('answers');
+
+    let numCorrect = 0;
+
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]: checked`;
+        const userAnswer = (answerContainer.querySelector(selector)||{}).value;
+
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        } else{
+            answerContainers[questionNumber].style.color = 'red';
+        }
+
+});
+
+resultsContainer.innerHTML = `${numCorrect} out of ${myQuestion.length}`;
+}
+
+
+
+submitBtn.addEventListener('click', showResults);
+
