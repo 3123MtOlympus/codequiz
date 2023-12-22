648b0295b3607339a48209984e21d6be27635082
3123MtOlympus <realchrislondon@gmail.com>
2023-12-21 17:32:33 -0800

    start button works

--- asset/script.js
+++ asset/script.js
@@ -1,30 +1,8 @@
 const startBtn = document.querySelector('button');
 
 function onStartBtnClick() {
-    alert();
-    const myQuestions = [{
-        question: "Javascript is a _______ langauge?",
-        answers: {
-        A: "Object-Oriented",
-        B: "Object-Based",
-        C: "Procedural",
-        D: "None",
-    },
-        correctAnswer: "A"
-},
-{
-    question:"Which of the following keywords is used to define a variable in Javascript?",
-    answers: {
-        A: "var",
-        B: "let",
-        C: "Both A and B",
-        D: "None of the Above",
-    },
-    correctAnswer: "C"
-},
-
-
-    ]
+    alert("Click OK to begin quiz.");
+    
 }
 
 startBtn.addEventListener('click', onStartBtnClick);
@@ -40,3 +18,57 @@ function showResults() { }
 buildQuiz();
 
 submitBtn.addEventListener('click', showResults);
+
+const myQuestions = [{
+    question: "Javascript is a _______ langauge?",
+    answers: {
+        A: "Object-Oriented",
+        B: "Object-Based",
+        C: "Procedural",
+        D: "None",
+    },
+    correctAnswer: "A"
+},
+{
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answers: {
+        A: "var",
+        B: "let",
+        C: "Both A and B",
+        D: "None of the Above",
+    },
+    correctAnswer: "C"
+},
+{
+    question: "Which of the following methods is used to access HTML elements using Javascript?",
+    answers: {
+        A: "getElementbyId",
+        B: "getElementsByClassName()",
+        C: "Both A and B",
+        D: "None of the Above",
+    },
+    correctAnswer: "C"
+},
+{
+    question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    answers: {
+        A: "Throws an error",
+        B: "ignores the statements",
+        C: "Giving a warning",
+        D: "None of the Above",
+    },
+    correctAnswer: "B"
+},
+{
+    question: "Which of the following methods can be used to display data in some form using Javascript?",
+    answers: {
+        A: "document.write()",
+        B: "console.log()",
+        C: "window.alert()",
+        D: "All of the above",
+    },
+    correctAnswer: "D"
+
+}
+
+];
