b1f44b3b96c3644eb92607386ef33d43835e394b
3123MtOlympus <realchrislondon@gmail.com>
2023-12-24 11:16:28 -0800

    adding scirot to new code

---
+++ script2.js
@@ -0,0 +1,7 @@
+const start_btn = document.querySelector(".start_btn button");
+const quiz_box = document.querySelector(".quizbox");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .timer_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
