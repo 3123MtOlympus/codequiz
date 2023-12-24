937c14fb455cae9d9a244beac98b5cf311fd31a3
3123MtOlympus <realchrislondon@gmail.com>
2023-12-24 11:59:18 -0800

    adding questins.js

---
+++ questions.js
@@ -0,0 +1,54 @@
+let questions = [
+    {
+        question: "Javascript is a _______ langauge.",
+        answer: "Object-Oriented",
+        options: [
+            "Object-Oriented",
+            "Object-Based",
+            "Procedural",
+            "None"
+        ]
+
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answer: "Both A and B",
+        options: [
+            "var",
+            "let",
+            "Both A and B",
+            "None of the Above"
+        ]
+
+    },
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        answer: "Both A and B",
+        options:[
+            "getElementbyId",
+            "getElementsByClassName()",
+            "Both A and B",
+            "None of the Above"
+        ]
+
+    },
+    {
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+       answer:  "ignores the statements",
+        options: [
+            "Throws an error", 
+            "ignores the statements", 
+            "Giving a warning", 
+            "None of the Above"]
+
+    },
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+       answer: "All of the above",
+        options: [
+            "document.write()", 
+            "console.log()", 
+            "window.alert()", 
+            "All of the above"]
+    },
+]
